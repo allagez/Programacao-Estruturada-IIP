6c14b35f74a00f3c69dc2d8ff3b6868a858e6a55
Allan Lucas <allslages@gmail.com>
2026-05-21 12:06:03 +0000

    Letra B 2ex

--- codes-in-class/Struct e Pointers/ExerciseStruct/2QuestaoStruct.c
+++ codes-in-class/Struct e Pointers/ExerciseStruct/2QuestaoStruct.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define CARACTERES 100
-#define TAM 40
+#define TAM 3
 
 struct Produto {
     int codigo;
@@ -37,7 +37,7 @@ void preencherStruct(struct Produto *p, int i){
     printf("\nDigite o valor unitário do produto: ");
     scanf("%lf", &p[i].v_unitario);
     printf("\nDigite a quantidade do estoque: ");
-    scanf("%d", p[i].qtd_estoque);
+    scanf("%d", &p[i].qtd_estoque);
 }
 
 void imprimirStruct(struct Produto *p, int tamanho){
@@ -46,7 +46,23 @@ void imprimirStruct(struct Produto *p, int tamanho){
         printf("Produto %d", i + 1);
         printf("\nCódigo: %d\n", p[i].codigo);
         printf("Descrição: %s\n", p[i].descricao);
+        printf("\n");
     }
+}
+
+int buscaSequencialProduto(int codigoBusca, struct Produto *p, int tamanho){
+    for (int i = 0; i < tamanho; i += 1){
+        if (p[i].codigo == codigoBusca){
+            return i;
+        }
+    }
+    printf("\nProduto não encontrado!\n");
+    return -1;
+}
+
+void alterarPreco(struct Produto *p, int tamanho, int i){
+    printf("\nDigite o novo valor: ");
+    scanf("%lf", &p[i].v_unitario);
 }
 
 int main(){
@@ -54,18 +70,33 @@ int main(){
     int entrada = 1;
     int i = 0;
     do {
-        printf("—————— MENU ——————\nSelecione:\n[1] — Cadastrar um novo produto\n[2] — Exibir código e descrição dos produtos\n[0] — Encerrar programa\nInsira uma opção válida: ");
+        printf("—————— MENU ——————\nSelecione:\n[1] — Cadastrar um novo produto\n[2] — Alterar valor unitário de produto\n[7] — Exibir código e descrição dos produtos\n[0] — Encerrar programa\nInsira uma opção válida: ");
         scanf("%d", &entrada);
+
         if (entrada == 1){
-            if (i >= 40){
+
+            if (i >= TAM){
                 printf("\nLIMITE DE DADOS ATINGIDO\n");
                 continue;
             }
-                preencherStruct(produto, i);
-                i += 1;
+            preencherStruct(produto, i);
+            i += 1;
+
         } else if (entrada == 2){
+
+            int codigoBusca, pos;
+            printf("\nInforme o código do produto que você deseja modificar o valor: ");
+            scanf("%d", &codigoBusca);
+            pos = buscaSequencialProduto(codigoBusca, produto, TAM);
+            if (pos != -1){
+                alterarPreco(produto, TAM, pos);
+            }
+
+        } else if (entrada == 7){
+
             imprimirStruct(produto, TAM);
             printf("\n");
+
         } else if (entrada != 0){
             printf("\nERRO: Insira uma opção válida!\n");
         }
